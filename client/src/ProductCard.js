c24a54a631ae1fd60c44e970db2773b176b31b47
ScottInPittsburgh <scottdrickman27@gmail.com>
2024-07-19 16:35:55 -0400

    Stylistic changes to Product tiles, Pt II

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -5,14 +5,13 @@ import './styles.css';
 const ProductCard = ({ _id, name, description, price, imageUrl, link }) => {
     return (
         <div className="product-card tile">
-            <Link to={link}>
-                <img src={imageUrl} alt={name} className="product-image" />
-                <div className="product-content">
-                    <h3>{name}</h3>
-                    <p>{description}</p>
-                    <p>Price: ${price.toFixed(2)}</p>
-                </div>
-            </Link>
+            <img src={imageUrl} alt={name} className="product-image" />
+            <div className="product-content">
+                <h3>{name}</h3>
+                <p>{description}</p>
+                <p>Price: ${price.toFixed(2)}</p>
+                <Link to={link} className="view-product-button">View Product</Link>
+            </div>
         </div>
     );
 };
