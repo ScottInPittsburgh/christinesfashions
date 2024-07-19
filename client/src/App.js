70e73638e6941149a27a16e220a2bbaac2570630
ScottInPittsburgh <scottdrickman27@gmail.com>
2024-07-19 12:26:15 -0400

    Changes to pull product tiles from DB

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Cart from './Cart';
-import ProductPage from './ProductPage';
+import Product from './Product';
 import CollectionPage from './CollectionPage';
 import Login from './Login';
 import Admin from './Admin';
@@ -16,7 +16,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/cart" element={<Cart />} />
-                    <Route path="/product/:id" element={<ProductPage />} />
+                    <Route path="/product/:id" element={<Product />} /> {/* Updated route */}
                     <Route path="/collection/:id" element={<CollectionPage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/admin" element={<Admin />} />
