9eca9813b2d7a7ac44b5f0aeb930d30486c3cd7f
ScottInPittsburgh <scottdrickman27@gmail.com>
2024-07-19 13:43:23 -0400

    Changes to pull product tiles from DB, pt VIII, added logging

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ProductCard = ({ _id, name, description, price, imageUrl }) => {
+const ProductCard = ({ id, name, description, price, imageUrl, link }) => {
     return (
         <div className="product-card">
-            <Link to={`/product/${_id}`}>
+            <Link to={link}>
                 <img src={imageUrl} alt={name} className="product-image" />
                 <h3>{name}</h3>
                 <p>{description}</p>
