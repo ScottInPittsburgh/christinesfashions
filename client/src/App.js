66ca15a461f0caa7ea6a9e19e434f988bc0f4933
ScottInPittsburgh <scottdrickman27@gmail.com>
2024-07-19 17:06:57 -0400

    Conditional formatting to remove header/footer from product page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './Home';
 import Cart from './Cart';
 import ProductPage from './ProductPage';
@@ -9,23 +9,95 @@ import Login from './Login';
 import Admin from './Admin';
 import AllProducts from './AllProducts';
 import Header from './Header';
+import Footer from './Footer';
 import './styles.css';
+
+const Layout = ({ children }) => {
+    const location = useLocation();
+    const noHeaderFooter = location.pathname === '/productpage/:id';
+
+    return (
+        <>
+            {!noHeaderFooter && <Header />}
+            {children}
+            {!noHeaderFooter && <Footer />}
+        </>
+    );
+};
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Header />
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/productpage/:id" element={<ProductPage />} />
-                    <Route path="/product/:id" element={<Product />} />
-                    <Route path="/collection/:id" element={<CollectionPage />} />
-                    <Route path="/collection/products" element={<AllProducts />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/admin" element={<Admin />} />
+                    <Route
+                        path="/"
+                        element={
+                            <Layout>
+                                <Home />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/home"
+                        element={
+                            <Layout>
+                                <Home />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/cart"
+                        element={
+                            <Layout>
+                                <Cart />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/productpage/:id"
+                        element={<ProductPage />}
+                    />
+                    <Route
+                        path="/product/:id"
+                        element={
+                            <Layout>
+                                <Product />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/collection/:id"
+                        element={
+                            <Layout>
+                                <CollectionPage />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/collection/products"
+                        element={
+                            <Layout>
+                                <AllProducts />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <Layout>
+                                <Login />
+                            </Layout>
+                        }
+                    />
+                    <Route
+                        path="/admin"
+                        element={
+                            <Layout>
+                                <Admin />
+                            </Layout>
+                        }
+                    />
                 </Routes>
             </div>
         </Router>
